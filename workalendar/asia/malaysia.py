edb94a4d020e164f1062e5f84407706d9a3b8594
abwqqq <150875888+abwqqq@users.noreply.github.com>
2024-05-08 17:18:17 +0800

    Update malaysia  .py
    update till 2025 and 2026 for date of thaipusam and deepavali

--- workalendar/asia/malaysia.py
+++ workalendar/asia/malaysia.py
@@ -52,6 +52,8 @@ class Malaysia(IslamicMixin, ChineseNewYearCalendar):
         2022: date(2022, 10, 24),
         2023: date(2023, 11, 13),  # in lieu of 12th Nov
         2024: date(2024, 10, 31),
+        2025: date(2025, 10, 20),
+        2026: date(2026, 11, 9), # in lieu of 8th Nov
     }
 
     # Ref: https://publicholidays.com.my/thaipusam/
@@ -71,6 +73,8 @@ class Malaysia(IslamicMixin, ChineseNewYearCalendar):
         2022: date(2022, 1, 18),
         2023: date(2023, 2, 4),
         2024: date(2024, 1, 25),
+        2025: date(2025, 2, 11),
+        2026: date(2026, 2, 2), # in lieu of 1st Feb
     }
     chinese_new_year_label = "First Day of Lunar New Year"
     include_chinese_second_day = True
